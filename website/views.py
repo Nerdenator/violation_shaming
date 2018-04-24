2cc4ccc5035addef416f1a47094d16e4905e48f6
Aaron McRuer <a.mcruer@live.com>
2018-04-23 20:08:47 -0500

    added first basic view

--- website/views.py
+++ website/views.py
@@ -3,7 +3,8 @@ from website.models import Violation
 
 
 # get list of counties with violations, ranked.
-def counties_with_violation_count():
-   # gets the Django ORM count for the violations in each county
-   county_count = {counted: Violation.objects.filter(county=counted).count() for counted in
+def counties_with_violation_count(request):
+    # gets the Django ORM count for the violations in each county
+    county_count = {counted: Violation.objects.filter(county=counted).count() for counted in
                     ('Platte', 'Clay', 'Cass', 'Jackson')}
+    return render(request, 'website/index.html', { 'violations_per_county': county_count })
