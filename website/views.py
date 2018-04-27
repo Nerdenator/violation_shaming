6c58cc7f029fe603dc09a57cec17d87e32b26301
Aaron McRuer <a.mcruer@live.com>
2018-04-26 21:13:19 -0500

    more work.

--- website/views.py
+++ website/views.py
@@ -3,8 +3,7 @@ from website.models import Violation
 
 
 def basic_county_data(request):
-    # county_count = violations_per_county()
     county_count = {counted: Violation.objects.filter(county=counted).filter(status='Open').count() for counted in
                     ('Platte', 'Clay', 'Cass', 'Jackson')}
-
+    jackson_violations = Violation.objects.filter(county="Jackson").filter(status='Open')
     return render(request, 'website/index.html', {'violations_per_county': county_count})
