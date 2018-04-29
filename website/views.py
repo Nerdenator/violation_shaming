ca995096999e31adaf8182da7a2358c558e95500
Aaron McRuer <a.mcruer@live.com>
2018-04-29 16:34:31 -0500

    added new table, needs model equivalent

--- website/views.py
+++ website/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from website.models import Violation
-
+from django.db import connection
 
 def basic_county_data(request):
     county_count = {counted: Violation.objects.filter(county=counted).filter(status='Open').count() for counted in
                     ('Platte', 'Clay', 'Cass', 'Jackson')}
-    jackson_violations = Violation.objects.filter(county="Jackson").filter(status='Open')
+
+
     return render(request, 'website/index.html', {'violations_per_county': county_count})
+
+
+def custom_sql_get_top_10_jackson(self):
+    with connection.cursor() as cursor:
+        cursor.execute()
