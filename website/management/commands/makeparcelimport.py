748905e87852b6de90b535a6e418935221fd9184
Aaron McRuer <a.mcruer@live.com>
2018-04-30 19:15:30 -0500

    moar management II: electric boogaloo

--- website/management/commands/makeparcelimport.py
+++ website/management/commands/makeparcelimport.py
@@ -1,7 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
-
+from pathlib import Path
 
 class Command(BaseCommand):
     help = 'Creates a PostgreSQL COPY statement using the header row of the parcel .csv file'
 
     def handle(self, *args, **options):
+        home = str(Path.home())
+        file = home + "/rows.csv"
+        try:
+            with open(file) as f:
+                headers = f.readline()
+                split_headers = headers.strip().split(",")
+                print(split_headers)
+        except Exception as ex:
+            print(ex)
+
