85e616e31e4f4aaeab3187c1bfad567059eb92cd
am034402 <aaron.mcruer@cerner.com>
2018-05-03 10:27:29 -0500

    more work.

--- website/management/commands/parcelimport.py
+++ website/management/commands/parcelimport.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from pathlib import Path
+from django.db import connection
+
 
 class Command(BaseCommand):
     help = 'Creates a PostgreSQL COPY statement using the header row of the parcel .csv file'
@@ -12,8 +14,14 @@ class Command(BaseCommand):
                 headers = f.readline()
                 split_headers = headers.strip().split(",")
                 print(split_headers)
-                # and now, to build a raw sql statement
+                myString = ", ".join(split_headers)
+                print(myString)
+                # and now, to build a raw sql statement to annoy Bandit.
+                query = "COPY website_parcel(" + myString + ") from '" + file + "' DELIMITER ',' CSV HEADER;"
+                print(query)
+                with connection.cursor() as cursor:
+                    cursor.execute(query)
+
 
         except Exception as ex:
             print(ex)
-
